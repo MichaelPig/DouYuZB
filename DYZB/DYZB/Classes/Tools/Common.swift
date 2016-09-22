7958c248976a7ae9ca2e86a39fc13111a6a120e8
13211147 <383974871@qq.com>
2016-09-22 15:35:48 +0800

    add bottomLine and scrollLine

---
+++ DYZB/DYZB/Classes/Tools/Common.swift
@@ -0,0 +1,14 @@
+//
+//  Common.swift
+//  DYZB
+//
+//  Created by michael on 16/9/21.
+//  Copyright © 2016年 Michael. All rights reserved.
+//
+
+import UIKit
+
+let kStatusBarH : CGFloat = 20
+let kNavigationBarH : CGFloat = 44
+let kScreenW = UIScreen.main.bounds.width
+let kScreenH = UIScreen.main.bounds.height
